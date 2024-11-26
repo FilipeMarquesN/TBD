d3efac0b2a3132ae73cd5eb5dab11769d1f38b2a
sprugit <126251504+sprugit@users.noreply.github.com>
2024-11-26 12:29:26 +0000

    fix: ignore mappings filename case

--- modules/querybuilder.py
+++ modules/querybuilder.py
@@ -10,7 +10,7 @@ def build_SQL_insert_queries(environment):
     mappings = [f for f in map_path.iterdir() if f.name[-5:] == '.json']
     queries = {}
     for file in mappings:
-        table_name = file.name[:-5]
+        table_name = file.name[:-5].lower()
         with open(str(file), "r") as map_file:
             attributes = load(map_file)
             queries[table_name] = f"INSERT INTO {table_name} VALUES ({", ".join([":"+attributes[i] for i in attributes])})" 
