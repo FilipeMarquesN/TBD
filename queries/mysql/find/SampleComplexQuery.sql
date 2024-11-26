111a19643bfb9c688115bc4461aa6ff9ba1a6039
Jessica Soares <jessica.sgsoares@gmail.com>
2024-11-26 12:02:46 +0000

    Some progress on hard queries

--- queries/mysql/find/SampleComplexQuery.sql
+++ queries/mysql/find/SampleComplexQuery.sql
@@ -11,10 +11,14 @@ heavy queries (sort by, group by, range queries)
 
 Atualizar o livro adicionar a tag 01_best-books que tiver mais reviews = 4 */
 
-SELECT b.id, COUNT(r.ratings) AS totalRatings
-FROM books b
-JOIN ratings r ON b.id =r.book_id
-Where r.ratings = 4
-GROUP BY b.id
-ORDER BY totalRatings DESC 
-LIMIT 1;
+UPDATE books /* adicionado */
+SET tags = CONCAT(tags, ',01_best-books') /* adicionado */
+WHERE id = ( /* adicionado */
+	SELECT b.id, COUNT(r.ratings) AS totalRatings
+	FROM books b
+	JOIN ratings r ON b.id =r.book_id
+	Where r.ratings = 4
+	GROUP BY b.id
+	ORDER BY totalRatings DESC 
+	LIMIT 1;
+); /* adicionado */
