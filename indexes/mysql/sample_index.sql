669d30826521f385e5563883f8d298dbaba663b3
Filipe Marques <80032520+FilipeMarquesN@users.noreply.github.com>
2024-12-02 10:54:13 +0000

    Update sample_index.sql

--- indexes/mysql/sample_index.sql
+++ indexes/mysql/sample_index.sql
@@ -1,3 +1,2 @@
-CREATE INDEX books_ratings ON books (Id,Rating);
-CREATE INDEX books_Authors ON books (Authors);
+CREATE INDEX books_ratings ON books (Id,Rating,Authors);
 CREATE INDEX books_Rating ON ratings(Rating);
