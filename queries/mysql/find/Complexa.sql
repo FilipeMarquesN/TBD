6f96ebe4ebe8fdae69c007a23832f68b2c9df750
Filipe Marques <Filipe.Nascimento.Marques1@gmail.com>
2024-12-13 19:42:16 +0000

    Changes na complexa :P pq me enganei no nome de 1 tabela

--- queries/mysql/find/Complexa.sql
+++ queries/mysql/find/Complexa.sql
@@ -1,50 +1,54 @@
 WITH MaiorFa AS (
-    SELECT u.UserId, b.id AS BookId, COUNT(r.UserId) AS ammount
+    SELECT 
+        u.UserId, 
+        b.id AS BookId, 
+        COUNT(r.UserId) AS amount
     FROM users u
-    JOIN rating r ON u.UserId = r.UserId
+    JOIN ratings r ON u.UserId = r.UserId
     JOIN books b ON r.BookId = b.id
     WHERE b.id IN (
-            SELECT BookId 
-            FROM Titles 
-            WHERE Title LIKE '%Harry Potter%'
-        )
+        SELECT BookId
+        FROM books
+        WHERE Title LIKE '%Harry Potter%'
+    )
     GROUP BY u.UserId, b.id
-    ORDER BY ammount
-    LIMIT 1
-
+    ORDER BY amount DESC
 )
+SELECT *
+FROM MaiorFa
+LIMIT 1;
 
 INSERT INTO books (
-    id, book_id, best_book_id, work_id, books_count, isbn, isbn13, authors, original_publication_year,
-    original_title, title, language_code, average_rating, ratings_count, work_ratings_count, 
-    work_text_reviews_count, ratings_1, ratings_2, ratings_3, ratings_4, ratings_5, image_url, small_image_url
+    id, book_id, best_book_id, work_id, books_count, isbn, isbn13, authors, 
+    original_publication_year, original_title, title, language_code, 
+    average_rating, ratings_count, work_ratings_count, work_text_reviews_count, 
+    ratings_1, ratings_2, ratings_3, ratings_4, ratings_5, image_url, small_image_url
 )
 SELECT 
-    1 AS id,                       
-    b.id AS book_id,               
-    b.best_book_id,                
-    b.work_id,                     
-    100 AS books_count,            
-    b.isbn,                        
-    b.isbn13,                      
-    b.authors,                     
-    b.original_publication_year,   
-    b.original_title,              
-    b.title,                       
-    b.language_code,               
-    b.average_rating,              
-    b.ratings_count,               
-    b.work_ratings_count,         
-    b.work_text_reviews_count,    
-    b.ratings_1,                   
-    b.ratings_2,                   
-    b.ratings_3,                   
-    b.ratings_4,                   
-    b.ratings_5,                   
-    b.image_url,                   
-    b.small_image_url             
+    999999 AS id,                      
+    999999 AS book_id,                 
+    999999 AS best_book_id,            
+    999999 AS work_id,                 
+    100 AS books_count,                
+    '999999' AS isbn,                  
+    '9999999999999' AS isbn13,         
+    mf.UserId AS authors,             
+    2024 AS original_publication_year, 
+    'Harry Potter Fan Book' AS original_title, 
+    'Harry Potter Fan Book' AS title,          
+    'eng' AS language_code,            
+    0 AS average_rating,               
+    0 AS ratings_count,                
+    0 AS work_ratings_count,           
+    0 AS work_text_reviews_count,      
+    0 AS ratings_1,                    
+    0 AS ratings_2,                    
+    0 AS ratings_3,                    
+    0 AS ratings_4,                    
+    0 AS ratings_5,                    
+    'None' AS image_url,               
+    'None' AS small_image_url          
 FROM 
-    books b
-JOIN 
-    MaiorFa mf ON b.id = mf.UserId;
-
+    MaiorFa mf;
+WHERE 
+    RowNum = 1;
